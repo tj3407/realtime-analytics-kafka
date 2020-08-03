a509f0d08fc014fb063592be63b11cef83f3c794
TJ Sornet <tsornet@gmail.com>
2020-08-03 12:43:57 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const URL = 'ws://localhost:3030'
 function App() {
+  const [ws, setWs] = React.useState(new WebSocket(URL));
+  const [message, setMessage] = React.useState(null);
+
+  React.useEffect(() => {
+    ws.onopen = () => {
+      console.log('connected');
+    }
+
+    ws.onmessage = (event) => {
+      console.log(event.data)
+      // const msg = JSON.parse(event.data);
+      // console.log(msg)
+      // setMessage(msg);
+    }
+
+    ws.onclose = () => {
+      console.log('disconnected');
+      setWs(new WebSocket(URL));
+    }
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* { message && message.length ? message.map(info => {
+        return (
+          <div>
+            {JSON.stringify(info)}
+          </div>
+        )
+      }) : "Retrieveing message..."} */}
     </div>
   );
 }
